80ad744648e340efc7f48082bbf846b0f6d0368c
Patricio Encina <patricio.encina@outlook.com>
2022-05-05 07:16:32 -0400

    Restaurants and Reviews excercise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,29 @@
 class ReviewsController < ApplicationController
 
   def show
-    @review = Review.find(params[:restaurant_id])
+    # @review = Review.find(params[:restaurant_id])
   end
 
   def new
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    #  @review.restaurant_id = @restaurant.id
+    if @review.save
+      redirect_to restaurant_path(@review)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
